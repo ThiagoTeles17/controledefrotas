f3ac3c5256d412426f4b5f31c79c0277438a2df3
Thiago Everton Teles <thiagoevertonteles@gmail.com>
2023-11-14 16:08:16 -0300

    14-11-2023_16:08

--- src/screens/Reports/Reports.jsx
+++ src/screens/Reports/Reports.jsx
@@ -5,27 +5,35 @@ import {Modal} from 'react-modal';
 
 import {AiFillCar} from 'react-icons/ai';
 import { BsPersonVcard } from 'react-icons/bs';
+import { FaTools } from "react-icons/fa";
+import { FaHistory } from "react-icons/fa";
+
 import { useState } from 'react';
+
+import { reportDrivers } from './Scripts/ReportDrivers';
 
 export const Reports = () => {
 
     const [modalVisible, setModalVisible] = useState(false);
 
-    const [modalContent, setModalContent] = useState('');
+    const [modalContent, setModalContent] = useState(<></>);
 
     return(
         <Container>
             <ReportBox icon={<AiFillCar size={80}/>} title="Veículos"/>
-            <ReportBox icon={<BsPersonVcard size={80}/>} title="Condutores"/>
-            <ReportBox icon={<AiFillCar size={80}/>} title="Emissão de Controle de Veículos em Massa"/>
+            <ReportBox handleOnClick={() => reportDrivers()} icon={<BsPersonVcard size={80}/>} title="Condutores"/>
+            <ReportBox icon={<AiFillCar size={80}/>} title="Controle de Veículos"/>
+            <ReportBox icon={<FaTools  size={80}/>} title="Manutenções Pendentes"/>
+            <ReportBox icon={<FaHistory  size={80}/>} title="Histórico de Manutenções"/>
 
-            <Modal
+
+            {/*<Modal
             isOpen={modalVisible}
             className={styles.modal}
             style={{overlay: {backgroundColor: 'rgba(214, 214, 214, 0.5)', zIndex: '10'}}}
             >   
             {modalContent}
-            </Modal> 
+            </Modal>*/} 
 
         </Container>
     );
