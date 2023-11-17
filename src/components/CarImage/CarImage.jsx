07e7f6f29447505e634855f98e7cd06489bec2d3
Thiago Everton Teles <thiagoevertonteles@gmail.com>
2023-11-16 21:03:36 -0300

    packeage

--- src/components/CarImage/CarImage.jsx
+++ src/components/CarImage/CarImage.jsx
@@ -1,6 +1,23 @@
+import { useContext, useEffect, useState } from 'react';
 import styles from './CarImage.module.css';
+import { ApiContext } from '../../context/ApiContext';
+import { doc, getDoc } from 'firebase/firestore';
 
 const CarImage = ({vehicles, curVehicle}) => {
+
+    const {db} = useContext(ApiContext);
+
+    const [images, setImages] = useState();
+
+    const getImage = async() => {
+        setImages((await getDoc(doc(db, 'imgs', 'vehicles'))).data());
+    };
+
+    useEffect(() => {
+        getImage();
+    }, []);
+
+    console.log(images[curVehicle] && images[curVehicle]);
 
     if(vehicles == null){
         return
@@ -10,7 +27,7 @@ const CarImage = ({vehicles, curVehicle}) => {
         <div className={styles.divContainer}>
             <img
                 className={styles.imageContainer} 
-                src={vehicles[curVehicle] ? `${vehicles[curVehicle].imagem}` : ''}
+                src={images[curVehicle] ? `${images[curVehicle]}` : ''}
                 alt='Imagem Carro'
             />
         </div>
